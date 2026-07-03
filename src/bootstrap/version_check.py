4b6ba347b3947b1f770eb0643eff2f3d1ce3bdea
PabloViniegra <pablovpmadrid@gmail.com>
2026-07-03 11:32:44 +0200

    chore(release): bump version to 0.1.2

--- src/bootstrap/version_check.py
+++ src/bootstrap/version_check.py
@@ -7,7 +7,7 @@ import urllib.request
 from typing import Callable
 
 
-CURRENT_VERSION = "0.1.1"
+CURRENT_VERSION = "0.1.2"
 
 _RELEASES_URL = "https://api.github.com/repos/PabloViniegra/drive-uploader/releases/latest"
 _DOWNLOAD_URL = "https://github.com/PabloViniegra/drive-uploader/releases/latest"
